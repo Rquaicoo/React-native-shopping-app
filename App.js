e7d8989adbac259a91de16c1b1f5d6387ee12344
Russell Quaicoo <russellquaicoo1@gmail.com>
2021-10-26 23:40:47 +0000

    Welcome page and login created

--- App.js
+++ App.js
@@ -1,22 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import signUp from "./screens/signUp";
-import logIn from './screens/login';
-import welcome from './screens/welcome';
 
+function logIn({navigation}){
+  return(
+      <View>
+        <Text>Hello Again!</Text>
+        <Text>Welcome back, you've {"\n"}   been missed</Text>
+      </View>
+  );
+}
+
+function welcome({navigation}){
+  return(
+      <View style = {styles.container}>
+        <Image
+        style={{
+          borderRadius: 20,
+          marginBottom: 60,
+          width: 300,
+          height: 190,
+        }}
+          source={require('./assets/sneaker-guide-6-784x441_adobespark.png')}
+       />
+        
+        <Text style={{fontWeight: "bold", color: "#ffffff", marginTop: 50, fontSize: 30}}>Let's improve your {"\n"}       appearance</Text>
+        <Text style={{color:"#ffffff", marginTop: 10}}>find cool shoes to improve your appearance</Text>
+        <TouchableOpacity
+         style={{backgroundColor: "#ffffff", paddingTop: 20, paddingBottom: 20, paddingLeft: 70, paddingRight: 70, borderRadius: 30, margin: 50}}
+         onPress={() => navigation.navigate('logIn')}>
+          <Text style={{fontWeight: "bold", fontSize: 20}}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+  );
+}
+
+function signUp({navigation}){
+  return(
+      <View><Text>Hello world</Text></View>
+  );
+}
 
 
 const Stack = createNativeStackNavigator();
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SignUp" component={signUp} options={{title: "Sign Up"}} />
-        <Stack.Screen name="Login" component={logIn} options={{title: "Login"}} />
-        <Stack.Screen name="" component={welcome} options={{title: "Welcome"}} />
+        <Stack.Screen name="welcome" component={welcome} options={{title: "Welcome"}} />
+        <Stack.Screen name="signUp" component={signUp} options={{title: "Sign Up"}} />
+        <Stack.Screen name="logIn" component={logIn} options={{title: "Login"}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -25,8 +60,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
--- screens/welcome.js
+++ screens/welcome.js
@@ -1,8 +1,3 @@
 import React from 'react'
-import {View} from "react-native"
+import {View, Text} from "react-native"
 
-export default function welcome(navigation){
-    return(
-        <View>Hello</View>
-    );
-}
